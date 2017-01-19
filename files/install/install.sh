6507667ae9c587dfbeb163357e8b78b039869f36
2devnull <2devnull@users.noreply.github.com>
2017-01-19 11:59:35 -0500

    Update install.sh

--- files/install/install.sh
+++ files/install/install.sh
@@ -23,48 +23,41 @@ rm -rf /etc/service/sshd /etc/my_init.d/00_regen_ssh_host_keys.sh
 #########################################
 
 # Repositories
-echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main universe restricted' > /etc/apt/sources.list
-echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main universe restricted' >> /etc/apt/sources.list
-echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt xenial main universe restricted' > /etc/apt/sources.list
+echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt xenial-updates main universe restricted' >> /etc/apt/sources.list
 
 # Install Dependencies
 apt-get update -qq
 # Install general
-apt-get install -qy --force-yes --no-install-recommends wget \
-                            				unzip
+apt-get install -qy --force-yes --no-install-recommends apt-utils \
+                                                        wget \
+							unzip \
+							dialog
 
 # Install window manager and x-server
-apt-get install -qy --force-yes --no-install-recommends x11-xserver-utils \
-                                                        libxrandr2 \
-                                                        libfuse2 \
-                                                        xutils \
-                                                        libxfixes3 \
-                                                        libx11-dev \
-                                                        libxml2 \
-                                                        zlib1g \
-                                                        fuse \
-                                                        autocutsel \
-                                                        pulseaudio \
-							x11-apps \
-                                                        openbox
-# x11rdp install
-dpkg -i /tmp/x11rdp/x11rdp_0.9.0+devel-1_amd64.deb
+apt-get install -qy --force-yes --no-install-recommends vnc4server \
+                                                        x11-xserver-utils \
+							openbox \
+							xfonts-base \
+							xfonts-100dpi \
+							xfonts-75dpi \
+							libfuse2
 
-# xrdp needs to be installed seperately
-dpkg -i /tmp/x11rdp/xrdp_0.9.0+devel_amd64.deb
+# Install xrdp
+apt-get install -qy --force-yes --no-install-recommends xrdp
+
 
 # Install Guac
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt-get install -y --force-yes oracle-java8-installer
-apt-get install -y --force-yes oracle-java8-set-default
-apt-get install -qy --force-yes --no-install-recommends libossp-uuid-dev \
-                                                        libpng12-dev \
-                                                        libfreerdp-dev \
-                                                        libcairo2-dev \
-                                                        tomcat7 \
-							lxterminal \
-							nano
+apt-get install -qy --force-yes --no-install-recommends default-jre \
+                                                        libossp-uuid-dev \
+							libpng12-dev \
+							libfreerdp-dev \
+							libcairo2-dev \
+							tomcat7
+
+# Install misc
+apt-get install -qy --force-yes --no-install-recommends nano \
+							lxterminal
 
 
 #########################################
@@ -88,41 +81,41 @@ EOT
 # user config
 cat <<'EOT' > /etc/my_init.d/01_user_config.sh
 #!/bin/bash
-
 USERID=${USER_ID:-99}
 GROUPID=${GROUP_ID:-100}
 groupmod -g $GROUPID users
 usermod -u $USERID nobody
 usermod -g $GROUPID nobody
 usermod -d /nobody nobody
-usermod -a -G adm,sudo,fuse nobody
 chown -R nobody:users /nobody/ 
 EOT
 
 # app config
 cat <<'EOT' > /etc/my_init.d/02_app_config.sh
 #!/bin/bash
-
 APPNAME=${APP_NAME:-"GUI_APPLICATION"}
-
 sed -i -e "s#GUI_APPLICATION#$APPNAME#" /etc/xrdp/xrdp.ini
 sed -i -e "s#GUI_APPLICATION#$APPNAME#" /etc/guacamole/noauth-config.xml
-
 if [[ -e /startapp.sh ]]; then 
-    chown nobody:users /startapp.sh
-    chmod +x /startapp.sh
+	chown nobody:users /startapp.sh
+	chmod +x /startapp.sh
 fi
 EOT
 
-# X11rdp
-mkdir -p /etc/service/X11rdp
-cat <<'EOT' > /etc/service/X11rdp/run
+
+# Xvnc
+
+
+# Xvnc
+mkdir -p /etc/service/Xvnc
+cat <<'EOT' > /etc/service/Xvnc/run
 #!/bin/bash
 exec 2>&1
 WD=${WIDTH:-1280}
 HT=${HEIGHT:-720}
-
-exec /sbin/setuser nobody X11rdp :1 -bs -ac -nolisten tcp -geometry ${WD}x${HT} -depth 16 -uds
+exec /sbin/setuser nobody Xvnc4 :1 -geometry ${WD}x${HT} -depth 16 -rfbwait 30000 -SecurityTypes None -rfbport 5901 -bs -ac \
+				   -pn -fp /usr/share/fonts/X11/misc/,/usr/share/fonts/X11/75dpi/,/usr/share/fonts/X11/100dpi/ \
+				   -co /etc/X11/rgb -dpi 96
 EOT
 
 # xrdp
@@ -131,25 +124,19 @@ cat <<'EOT' > /etc/service/xrdp/run
 #!/bin/bash
 exec 2>&1
 RSAKEYS=/etc/xrdp/rsakeys.ini
-
     # Check for rsa key
-    [ -f /usr/share/doc/xrdp/rsakeys.ini ] && rm /usr/share/doc/xrdp/rsakeys.ini
-    ln -s $RSAKEYS /usr/share/doc/xrdp/rsakeys.ini
-    if [ ! -f $RSAKEYS ]; then
+    if [ ! -f $RSAKEYS ] || cmp $RSAKEYS /usr/share/doc/xrdp/rsakeys.ini > /dev/null; then
         echo "Generating xrdp RSA keys..."
         (umask 077 ; xrdp-keygen xrdp $RSAKEYS)
         chown root:root $RSAKEYS
         if [ ! -f $RSAKEYS ] ; then
-	        echo "could not create $RSAKEYS"
+            echo "could not create $RSAKEYS"
             exit 1
         fi
+        echo "done"
     fi
-    [ -f /var/run/xrdp/xrdp.pid ] && rm /var/run/xrdp/xrdp.pid
-    echo "Starting xrdp!"
-
 exec /usr/sbin/xrdp --nodaemon
 EOT
-
 
 # xrdp.ini
 cat <<'EOT' > /etc/xrdp/xrdp.ini
@@ -171,125 +158,94 @@ bulk_compression=yes
 new_cursors=yes
 use_fastpath=both
 hidelogwindow=yes
-
-[Logging]
-LogFile=xrdp-ng.log
-LogLevel=DEBUG
-EnableSyslog=1
-SyslogLevel=DEBUG
-
+[xrdp1]
+name=GUI_APPLICATION
+lib=libvnc.so
+username=nobody
+password=PASSWD
+ip=127.0.0.1
+port=5901
 [channels]
 rdpdr=true
 rdpsnd=true
 drdynvc=true
 cliprdr=true
 rail=true
-
-[xrdp1]
-name=GUI_APPLICATION
-lib=libxup.so
-username=na
-password=na
-ip=127.0.0.1
-port=/tmp/.xrdp/xrdp_display_1
-chansrvport=/tmp/.xrdp/xrdp_chansrv_socket_1
-xserverbpp=16
-code=10
-
 EOT
 
-# xrdp-chansrv
-mkdir -p /etc/service/xrdp-chansrv
-cat <<'EOT' > /etc/service/xrdp-chansrv/run
+# xrdp-sesman
+mkdir -p /etc/service/xrdp-sesman
+cat <<'EOT' > /etc/service/xrdp-sesman/run
 #!/bin/bash
-
-exec env DISPLAY=:1 HOME=/nobody /sbin/setuser nobody xrdp-chansrv
+exec 2>&1
+exec /usr/sbin/xrdp-sesman --nodaemon >> /var/log/xrdp-sesman_run.log 2>&1
 EOT
 
-# autocutsel
-mkdir -p /etc/service/autocutsel
-cat <<'EOT' > /etc/service/autocutsel/run
-#!/bin/bash
-
-exec env DISPLAY=:1 HOME=/nobody /sbin/setuser nobody autocutsel
+# sesman.ini
+cat <<'EOT' > /etc/xrdp/sesman.ini
+[Globals]
+ListenAddress=127.0.0.1
+ListenPort=3350
+EnableUserWindowManager=1
+UserWindowManager=startwm.sh
+DefaultWindowManager=startwm.sh
+[Security]
+AllowRootLogin=1
+MaxLoginRetry=4
+TerminalServerUsers=tsusers
+TerminalServerAdmins=tsadmins
+AlwaysGroupCheck = false
+[Sessions]
+X11DisplayOffset=10
+MaxSessions=1
+KillDisconnected=0
+IdleTimeLimit=0
+DisconnectedTimeLimit=0
+Policy=Default
+[Logging]
+LogFile=xrdp-sesman.log
+LogLevel=DEBUG
+EnableSyslog=1
+SyslogLevel=DEBUG
+[Xvnc]
+param1=-bs
+param2=-ac
+param5=-localhost
+param6=-dpi
+param7=96
 EOT
 
-# autocutsel2
-mkdir -p /etc/service/autocutsel2
-cat <<'EOT' > /etc/service/autocutsel2/run
-#!/bin/bash
-
-exec env DISPLAY=:1 HOME=/nobody /sbin/setuser nobody autocutsel -selection PRIMARY
-EOT
-
-# xclipboard
-mkdir -p /etc/service/xclipboard
-cat <<'EOT' > /etc/service/xclipboard/run
-#!/bin/bash
-sv -w7 check openbox
-
-exec env DISPLAY=:1 HOME=/nobody /sbin/setuser nobody xclipboard
-EOT
-
-# asound.conf
-cat <<'EOT' > /etc/asound.conf
-pcm.pulse {
-    type pulse
-}
-
-ctl.pulse {
-    type pulse
-}
-
-pcm.!default {
-    type pulse
-}
-
-ctl.!default {
-    type pulse
-}
-EOT
-
-# pulseaudio
-mkdir -p /etc/service/pulseaudio
-cat <<'EOT' > /etc/service/pulseaudio/run
-#!/bin/bash
-
-exec env DISPLAY=:1 HOME=/nobody /sbin/setuser nobody pulseaudio -F /etc/xrdp/pulse/default.pa -n
-EOT
 
 # openbox
 mkdir -p /etc/service/openbox
 cat <<'EOT' > /etc/service/openbox/run
 #!/bin/bash
 exec 2>&1
-
 exec env DISPLAY=:1 HOME=/nobody /sbin/setuser nobody  /usr/bin/openbox-session
 EOT
+
+
+
 
 mkdir -p /etc/service/tomcat7
 cat <<'EOT' > /etc/service/tomcat7/run
 #!/bin/bash
 exec 2>&1
-
 touch /var/lib/tomcat7/logs/catalina.out
-
 cd /var/lib/tomcat7
-
-exec java -Djava.util.logging.config.file=/var/lib/tomcat7/conf/logging.properties \
-          -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager \
-          -Djava.awt.headless=true -Xmx128m -XX:+UseConcMarkSweepGC \
-          -Djava.endorsed.dirs=/usr/share/tomcat7/endorsed \
-          -classpath /usr/share/tomcat7/bin/bootstrap.jar:/usr/share/tomcat7/bin/tomcat-juli.jar \
-          -Dcatalina.base=/var/lib/tomcat7 -Dcatalina.home=/usr/share/tomcat7 \
-          -Djava.io.tmpdir=/tmp/tomcat7-tomcat7-tmp org.apache.catalina.startup.Bootstrap start
+exec /usr/lib/jvm/java-8-oracle/jre/bin/java -Djava.util.logging.config.file=/var/lib/tomcat7/conf/logging.properties \
+                                           -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager \
+                                           -Djava.awt.headless=true -Xmx128m -XX:+UseConcMarkSweepGC \
+                                           -Djava.endorsed.dirs=/usr/share/tomcat7/endorsed \
+                                           -classpath /usr/share/tomcat7/bin/bootstrap.jar:/usr/share/tomcat7/bin/tomcat-juli.jar \
+                                           -Dcatalina.base=/var/lib/tomcat7 -Dcatalina.home=/usr/share/tomcat7 \
+                                           -Djava.io.tmpdir=/tmp/tomcat7-tomcat7-tmp org.apache.catalina.startup.Bootstrap start
 EOT
 
 mkdir -p /etc/service/guacd
 cat <<'EOT' > /etc/service/guacd/run
 #!/bin/bash
 exec 2>&1
-
 exec /usr/local/sbin/guacd -f
 EOT
 
@@ -297,14 +253,11 @@ mkdir -p /etc/guacamole
 cat <<'EOT' > /etc/guacamole/guacamole.properties
 # Location to read extra .jar's from (don't change for this docker config)
 lib-directory:  /var/lib/guacamole/classpath
-
 # Hostname and port of guacamole proxy (don't change for this docker config)
 guacd-hostname: localhost
 guacd-port:     4822
-
 # Auth provider class
 auth-provider: net.sourceforge.guacamole.net.auth.noauth.NoAuthenticationProvider
-
 # NoAuth properties
 noauth-config: /etc/guacamole/noauth-config.xml
 EOT
@@ -323,13 +276,13 @@ EOT
 # Openbox User nobody autostart
 cat <<'EOT' > /nobody/.config/openbox/autostart
 # Programs that will run after Openbox has started
-
 xsetroot -solid black -cursor_name left_ptr
 if [ -e /startapp.sh ]; then 
-    echo "Starting X app..."
-    exec /startapp.sh
+	echo "Starting X app..."
+ 	exec /startapp.sh
 fi
 EOT
+
 
 chmod -R +x /etc/service/ /etc/my_init.d/ 
 
@@ -345,12 +298,12 @@ mkdir -p /usr/share/tomcat7-root/.guacamole
 mkdir -p /root/.guacamole
 
 # Install guacd
-dpkg -i /tmp/guacamole/guacamole-server_0.9.7_amd64.deb
+dpkg -i /tmp/guacamole/guacamole-server_0.9.9_amd64.deb
 ldconfig
 
 # Configure tomcat
-cp /tmp/guacamole/guacamole-0.9.7.war /var/lib/tomcat7/webapps/guacamole.war
-cp /tmp/guacamole/guacamole-auth-noauth-0.9.7.jar /var/lib/guacamole/classpath
+cp /tmp/guacamole/guacamole-0.9.9.war /var/lib/tomcat7/webapps/guacamole.war
+cp /tmp/guacamole/guacamole-auth-noauth-0.9.9.jar /var/lib/guacamole/classpath
 ln -s /etc/guacamole/guacamole.properties /usr/share/tomcat7/.guacamole/
 ln -s /etc/guacamole/guacamole.properties /usr/share/tomcat7-root/.guacamole/
 ln -s /etc/guacamole/guacamole.properties /root/.guacamole/
@@ -360,22 +313,25 @@ rm -Rf /var/lib/tomcat7/webapps/ROOT
 ln -s /var/lib/tomcat7/webapps/guacamole.war /var/lib/tomcat7/webapps/ROOT.war 
 
 ### Compensate for GUAC-513
-ln -s /usr/local/lib/freerdp/guacsnd.so /usr/lib/x86_64-linux-gnu/freerdp/ 
+mkdir -p /usr/lib/x86_64-linux-gnu/freerdp
+ln -s /usr/local/lib/freerdp/guacsnd.so /usr/lib/x86_64-linux-gnu/freerdp/
 ln -s /usr/local/lib/freerdp/guacdr.so /usr/lib/x86_64-linux-gnu/freerdp/
 
 # openbox confg
 cp /tmp/openbox/rc.xml /nobody/.config/openbox/rc.xml
 chown nobody:users /nobody/.config/openbox/rc.xml
 
-# pulseauido rdp
-cp /tmp/x11rdp/module-xrdp* /usr/lib/pulse-4.0/modules
-chown -R 777 /usr/lib/pulse-4.0/modules
-
 #########################################
 ##                 CLEANUP             ##
 #########################################
 
 # Clean APT install files
-apt-get autoremove -y 
 apt-get clean -y
-rm -rf /var/lib/apt/lists/* /var/cache/* /var/tmp/* /tmp/guacamole /tmp/openbox /tmp/x11rdp
+apt-get autoclean -y
+apt-get autoremove -y
+rm -rf /usr/share/locale/*
+rm -rf /var/cache/debconf/*-old
+rm -rf /var/lib/apt/lists/*
+rm -rf /usr/share/doc/*
+rm -rf /tmp/* /var/tmp/*
+rm -rf /var/lib/apt/lists/* /var/cache/*
